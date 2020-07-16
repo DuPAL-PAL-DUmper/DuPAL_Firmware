c77e8391922a2ca7073f6683e0e5d0d04c053a7a
Baglio Tabifata <tabaglio@posteo.net>
2020-07-16 13:44:36 +0200

    Configure MCU ports

--- src/libs/ioconfig/ioconfig.c
+++ src/libs/ioconfig/ioconfig.c
@@ -8,5 +8,10 @@ void io_init() {
     // Config the shifter ports
     SHFT_DDR_1 |= (_BV(SHFT_1_SER) | _BV(SHFT_1_CLK) | _BV(SHFT_1_RST)); // Configure these pins as output
     SHFT_DDR_2 |= (_BV(SHFT_2_RCLK) | _BV(SHFT_2_OE));
+
+    // Set the other ports for PAL communication as IN or OUT
+    DDRC &= ~(_BV(2) | _BV(3) | _BV(4) | _BV(5)); // PC2-5 as inputs
+    DDRD &= ~(_BV(2) | _BV(3) | _BV(4) | _BV(5)); // PD2-5 as inputs
+    DDRD |= (_BV(6) | _BV(7)); // PD6,7 as outputs
 }
 
